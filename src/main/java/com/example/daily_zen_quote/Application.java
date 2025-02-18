12cce860932080cd014a964174cc87a33b2fc2a5
bemar24 <joa.berriel.24@lehre.mosbach.dhbw.de>
2025-02-18 14:44:38 +0100

    final (for now)

--- src/main/java/com/example/daily_zen_quote/Application.java
+++ src/main/java/com/example/daily_zen_quote/Application.java
@@ -1,7 +1,9 @@
 package com.example.daily_zen_quote;
 
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -16,8 +18,10 @@ public class Application extends javafx.application.Application {
     public void start(Stage stage) throws IOException {
         Font.loadFont(getClass().getResourceAsStream("/fonts/Parisienne-Regular.ttf"), 30);
 
-
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("username-view.fxml"));
+
+
+
         Scene scene = new Scene(fxmlLoader.load(), 600 , 400);
         stage.setTitle("Username");
         stage.setScene(scene);
